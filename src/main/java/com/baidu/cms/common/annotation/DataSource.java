9a6e0d51137176c43a21c13735a52263ead5cc93
v_shiyanjun <v_shiyanjun@baidu.com>
2018-10-10 15:52:53 +0800

    注解式数据源

---
+++ src/main/java/com/baidu/cms/common/annotation/DataSource.java
@@ -0,0 +1,21 @@
+package com.baidu.cms.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *  注解式数据源，用来进行数据源切换
+ *
+ *  @author: shiyanjun
+ *  @Date: 2018/10/10 上午11:54
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DataSource {
+
+    String value() default "";
+}
